1779786f08af38f44abaab6c0fea421e2644f041
Westbrook <2510950G@student.gla.ac.uk>
2023-01-03 11:47:16 +0800

    prepare for test on nodes

--- test.py
+++ test.py
@@ -10,15 +10,15 @@ from mpt import MerklePatriciaTrie
 # print(old_trie.root().hex())
 # root = old_trie.root()
 
-root = binascii.unhexlify('1f877b48a1e60d54a41747e5c674a6ced40353480c5ad3fcbee94f8ea3322fac')
-print(root)
-# root = binascii.unhexlify('ca83206464856464646664')
+# root = binascii.unhexlify('1f877b48a1e60d54a41747e5c674a6ced40353480c5ad3fcbee94f8ea3322fac')
 # print(root)
-trie = Level1db(root=root)
-
-for n in range(10):
-    value = f'df{n}'
-    print(trie.get(value.encode()))
+# # root = binascii.unhexlify('ca83206464856464646664')
+# # print(root)
+# trie = Level1db(root=root)
+#
+# for n in range(10):
+#     value = f'df{n}'
+#     print(trie.get(value.encode()))
 # sender = str(0)
 # trie.update(b'dd', b'dddfd')
 # print(trie.root().hex())
@@ -30,5 +30,10 @@ for n in range(10):
 # print("Root is {}".format(old_root))
 # print("Root hash is {}".format(old_root_hash.hex()))
 
+def create_file(string):
+    with open('./test', 'w') as f:
+        f.write(string)
 
 
+with open("test/node1/data/baseCoin.txt", "r") as f:
+    print(f.read())
