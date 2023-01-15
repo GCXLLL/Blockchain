4e279e9f855c698ae422660c60b1bef058346f8a
Westbrook <2510950G@student.gla.ac.uk>
2023-01-15 17:51:49 +0800

    try a css template

---
+++ web/web.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+# from flask_bootstrap import Bootstrap4
+
+app = Flask(__name__)
+# bootstrap = Bootstrap4(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005)
