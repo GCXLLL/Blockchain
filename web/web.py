287a75236b2ea991e5bf580cb6df9215d475c5c7
Westbrook <2510950G@student.gla.ac.uk>
2023-01-16 11:51:05 +0800

    modify the sections and name of html

--- web/web.py
+++ web/web.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
 # from flask_bootstrap import Bootstrap4
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static')
 # bootstrap = Bootstrap4(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+
+@app.route('/notarization')
+def notarization():
+    return render_template('notarization.html')
+
+
+@app.route('/chain')
+def chain():
+    return render_template('chain.html')
+
+
+@app.route('/management')
+def management():
+    return render_template('management.html')
 
 
 if __name__ == '__main__':
